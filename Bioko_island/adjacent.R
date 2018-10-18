4ae1d1d2d0f9012fabd556ddbce88563f8a12562
Qian Zhang <zqstat@gmail.com>
2018-10-18 12:13:47 -0700

    histgram

--- Bioko_island/adjacent.R
+++ Bioko_island/adjacent.R
@@ -5,3 +5,11 @@ g <- igraph::graph.adjacency(ad_cell)
 clu <- igraph::components(g)
 
 group_information <- igraph::groups(clu)
+
+csize <- length(group_information)
+com_cluster <- rep(NA, csize)
+for (i in 1:csize){
+  com_cluster[i] <- length(group_information[[i]])
+}
+
+hist(log(com_cluster, 10))
