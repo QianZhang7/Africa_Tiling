c5841e027e0e5ec41b6364b585a4643c39048770
Qian Zhang <zqstat@gmail.com>
2018-10-22 14:26:53 -0700

    split by urbanicity

--- Bioko_island/largest_blob.R
+++ Bioko_island/largest_blob.R
@@ -1,2 +1,12 @@
 large_id <- which(com_cluster == 1920)
 urban_ele <- as.numeric(unlist(group_information[large_id]))
+urban_poly <- shp_15[urban_ele,]
+urban_poly$urban <- rep(NA, 1920)
+
+for (i in 1:1920){
+  if(urban_poly$pop[i] >= 100){
+    urban_poly$urban[i] <- 1
+  }else{
+    urban_poly$urban[i] <- 0
+  }
+}
