9518529c80bca62dd7337bff29e13b9942843c0c
Qian Zhang <zqstat@gmail.com>
2018-12-18 14:27:32 -0800

    Update Bioko_island/read_all_data.R

--- Bioko_island/read_all_data.R
+++ Bioko_island/read_all_data.R
@@ -13,14 +13,20 @@ world_100_2015 <- raster("/Users/qianzh/project/Tiling/data/population/Equatoria
 world_100_2010 <- crop(world_100_2010, extent(mcdi100_shp))
 world_100_2015 <- crop(world_100_2015, extent(mcdi100_shp))
 
+# read mcdi 1km
+mcdi1k_shp <- readOGR("project/Tiling/data/population/mcdi_1km/popareas.shp")
+mcdi1k_shp <- spTransform(mcdi1k_shp, CRS("+proj=longlat +datum=WGS84"))
+#proj4string(mcdi1k_shp) <- CRS("+proj=utm +zone=32 +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs +towgs84:0,0,0")
+
+
 #read worldpop 1km raster
 world_1k_2010 <- raster("project/Tiling/data/population/worldpop2010/AFR_PPP_2010_adj_v2.tif")
-world_1k_2010 <- crop(world_1k_2010, extent(mcdi100_shp))
+world_1k_2010 <- crop(world_1k_2010, extent(mcdi1k_shp))
 world_1k_2015 <- raster("project/Tiling/data/population/worldpop2015/AFR_PPP_2015_adj_v2.tif")
-world_1k_2015 <- crop(world_1k_2015, extent(mcdi100_shp))
+world_1k_2015 <- crop(world_1k_2015, extent(mcdi1k_shp))
 
 # read landscan 1km
 landscan <- raster("project/Tiling/data/popgrid/LandScan Global 2017/lspop2017/dblbnd.adf")
-landscan <- crop(landscan, extent(mcdi100_shp))
+landscan <- crop(landscan, extent(mcdi1k_shp))
 
 save.image("project/Tiling/data/population/population_rasters.RData")
