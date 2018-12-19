9da0c3bc26d41c629330bac653464df770563c1d
Qian Zhang <zqstat@gmail.com>
2018-12-19 12:08:02 -0800

    update

--- Bioko_island/alldata_raster_plot.R
+++ Bioko_island/alldata_raster_plot.R
@@ -3,7 +3,6 @@ library(rgdal)
 library(raster)
 library(RColorBrewer)
 
-
 # load the data image
 load("project/Tiling/data/population/population_rasters.RData")
 
@@ -16,18 +15,23 @@ r_1k <- raster(ncol=57, nrow=63)
 extent(r_1k) <- extent(world_1k_2010)
 r_mcdi1k <- rasterize(mcdi1k_shp, r_1k, field = 'pop', update=TRUE, updateValue="NA")
 
-r_mcdi100[is.na(r_mcdi100[])] <- -9999
-
-# viz the rasters
-cuts=c(0,1,10,100,1000,10000,100000)
-world.shp <- readOGR("project/Tiling/data/population/TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp")
-
 # viz the 100m rasters
 par(mfrow = c(1,2))
+cuts=c(0,1,10,100,1000)
+png("project/Tiling/data/popgrid/dec2018/mcdi_100_raster.png", height = 800, width = 1200)
+plot(r_mcdi100, xlim = c(8.4, 9), ylim = c(3.2,3.8), 
+     main = "MCDI(100m) for Bioko", breaks= cuts, 
+     col=c("grey", brewer.pal(n = 4, name = "YlOrRd")), legend = F)
+plot(world_100_2010, col=c("gray50"), legend = F, add = T)
+plot(r_mcdi100, xlim = c(8.4, 9), ylim = c(3.2,3.8), 
+     main = "MCDI(100m) for Bioko", breaks= cuts, 
+     col=c("grey", brewer.pal(n = 4, name = "YlOrRd")), legend = F, add = T)
+legend(x = "bottomright",inset = 0, y = 3.4,
+       legend = c("0","1","10","100","1000","10000","100000"),
+       col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), lwd=5, cex=.5, horiz = F)
 
-plot(world.shp, col = "gray80")
-plot(r_mcdi100, main = "MCDI(100m) for Bioko", breaks= cuts, col=c("grey", brewer.pal(n = 6, name = "YlOrRd")), legend = F, add = T)
+plot(world_100_2010, xlim = c(8.4, 9), ylim = c(3,4), main = "WorldPOP_2010(100m) for Bioko", breaks= cuts, col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), legend = F)
 legend(x = "bottomright",inset = 0,
-       legend = c("0","1","10","100","1000","10000","100000"), 
-       col=c("grey", brewer.pal(n = 7, name = "YlOrRd")), lwd=5, cex=.5, horiz = F)
-
+       legend = c("0","1","10","100","1000"), 
+       col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), lwd=5, cex=.5, horiz = F)
+dev.off()
---
+++ Bioko_island/plot_raster3.R
@@ -0,0 +1,52 @@
+load("project/Tiling/data/population/population_rasters.RData")
+
+# convert shapefile to raster # mcdi
+r_100 <- raster(ncol=568, nrow=626)
+extent(r_100) <- extent(world_100_2010)
+r_mcdi100 <- rasterize(mcdi100_shp, r_100, field = 'PEOPLE_N', update=TRUE, updateValue="NA")
+
+r_1k <- raster(ncol=57, nrow=63)
+extent(r_1k) <- extent(world_1k_2010)
+r_mcdi1k <- rasterize(mcdi1k_shp, r_1k, field = 'pop', update=TRUE, updateValue="NA")
+
+png("project/Tiling/data/popgrid/dec2018/mcdi_worldpop_100_raster.png", height = 800, width = 1200)
+par(mfrow = c(2,2))
+plot(world_pop_2010, xlim = c(8.4, 9), ylim = c(3,4), main = "WorldPOP(100m) for Bioko", breaks= cuts, col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), legend = F)
+legend(x = 9,inset = 0, y = 3.4,
+       legend = c("0","1","10","100","1000"), 
+       col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), lwd=10, cex=1, horiz = F)
+plot(r_mcdi100, xlim = c(8.4, 9), ylim = c(3,4), 
+     main = "MCDI(100m) for Bioko", breaks= cuts, 
+     col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), legend = F)
+plot(world_100_2010, col=c("gray50"), legend = F, add = T)
+plot(r_mcdi100, xlim = c(8.4, 9), ylim = c(3,4), 
+     main = "MCDI(100m) for Bioko", breaks= cuts, 
+     col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), legend = F, add = T)
+legend(x = 8.98,inset = 0,y = 3.4,
+       legend = c("0","1","10","100","1000"), 
+       col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), lwd=10, cex=1, horiz = F)
+dev.off()
+
+# 1km
+png("project/Tiling/data/popgrid/dec2018/mcdi_worldpop_1k_raster.png", height = 800, width = 1200)
+par(mfrow = c(2,2))
+plot(landscan, xlim = c(8.4, 9), ylim = c(3,4), main = "LandScan(1km) for Bioko", breaks= cuts, col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), legend = F)
+legend(x = 9, y = 3.5,inset = 0,
+       legend = c("0","1","10","100","1000","10000","100000"), 
+       col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), lwd=5, cex=1, horiz = F)
+plot(world_1k_2010, xlim = c(8.4, 9), ylim = c(3,4), main = "WorldPOP(1km) for Bioko", breaks= cuts, col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), legend = F)
+legend(x = 9, y = 3.5,inset = 0,
+       legend = c("0","1","10","100","1000","10000","100000"), 
+       col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), lwd=5, cex=1, horiz = F)
+plot(r_mcdi1k, xlim = c(8.4, 9), ylim = c(3,4), 
+     main = "MCDI(1km) for Bioko", breaks= cuts, 
+     col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), legend = F)
+plot(world_1k_2010, col=c("gray50"), legend = F, add = T)
+plot(r_mcdi1k, xlim = c(8.4, 9), ylim = c(3,4), 
+     main = "MCDI(1km) for Bioko", breaks= cuts, 
+     col=c("gray50", brewer.pal(n = 4, name = "YlOrRd")), legend = F, add = T)
+legend(x = 8.98, y = 3.5,inset = 0,
+       legend = c("0","1","10","100","1000","10000","100000"), 
+       col=c("gray50", brewer.pal(n = 6, name = "YlOrRd")), lwd=5, cex=1, horiz = F)
+dev.off()
+
