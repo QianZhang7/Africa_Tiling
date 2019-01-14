c883c554b0f55d2cc0af1a2d76634e4f0ffedd10
Qian Zhang <zqstat@gmail.com>
2019-01-14 10:57:30 -0800

    Create raster_aggregated.R

---
+++ Bioko_island/raster_aggregated.R
@@ -0,0 +1,31 @@
+load("project/Tiling/data/population/population_rasters.RData")
+
+world_100_value <- values(world_100_2010)
+#world_pop_bioko <- world_pop_bioko[!is.na(world_pop_bioko)]
+world_100_value[is.na(world_100_value)] <- 0
+
+world_1k_value <- values(world_1k_2010)
+#world_pop_bioko <- world_pop_bioko[!is.na(world_pop_bioko)]
+world_1k_value[is.na(world_1k_value)] <- 0
+
+landscan_pop <- values(landscan)
+#landscan_pop <- landscan_pop[!is.na(landscan_pop)]
+landscan_pop[is.na(landscan_pop)] <- 0
+
+world_200_2010 <- aggregate(world_100_2010,fact=2,fun=sum)
+world_500_2010 <- aggregate(world_100_2010,fact=5,fun=sum)
+
+library(rgeos)
+mcdi1k_centers <- SpatialPointsDataFrame(gCentroid(mcdi1k_shp, byid=TRUE),data.frame(as.numeric(as.character(mcdi1k_shp$pop))), match.ID=FALSE)
+r_1k_grid <- raster(ncols=57, nrows=63)
+extent(r_1k_grid) <- extent(mcdi1k_shp)
+raster_mcdi_1k <- rasterize(mcdi1k_centers, r_1k_grid, field = "as.numeric.as.character.mcdi1k_shp.pop..")
+
+mcdi100_centers <- SpatialPointsDataFrame(gCentroid(mcdi100_shp, byid=TRUE),data.frame(mcdi100_shp$PEOPLE_N), match.ID=FALSE)
+r_100_grid <- raster(ncols=568, nrows=626)
+extent(r_100_grid) <- extent(mcdi100_shp)
+raster_mcdi_100 <- rasterize(mcdi100_centers, r_100_grid, field = "mcdi100_shp.PEOPLE_N")
+
+mcdi_200_2010 <- aggregate(raster_mcdi_100,fact=2,fun=sum)
+mcdi_500_2010 <- aggregate(raster_mcdi_100,fact=5,fun=sum)
+
