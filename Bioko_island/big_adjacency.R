673463ffa675ad5c7aa4018341b1ef76f8fc4b4a
Qian Zhang <zqstat@gmail.com>
2019-01-17 09:39:30 -0800

    Create big_adjacency.R

---
+++ Bioko_island/big_adjacency.R
@@ -0,0 +1,14 @@
+world_1k_2010_shp = rasterToPolygons(world_1k_2010)
+ad_world_1k_2010_shp = gTouches(world_1k_2010_shp, world_1k_2010_shp,  byid = TRUE)
+world_100_2010_shp = rasterToPolygons(world_100_2010)
+ad_world_100_2010_shp = gTouches(world_100_2010_shp[1:10000,], world_100_2010_shp[10001:20000,],  byid = TRUE)
+
+# polyids = seq_along(world_1k_2010_shp)
+# adjMat = matrix(FALSE, ncol = length(world_1k_2010_shp), nrow = length(world_1k_2010_shp))
+# for (ii in polyids) {
+#   for (jj in setdiff(polyids, seq_len(ii))) {
+#     adjMat[ii, jj] = 
+#       class(gIntersection(world_1k_2010_shp[ii, ], world_1k_2010_shp[jj, ])) == 'SpatialLines'
+#   }
+# }
+ad_world_1k_2010_shp2 = gOverlaps(world_1k_2010_shp, world_1k_2010_shp,  byid = TRUE)
