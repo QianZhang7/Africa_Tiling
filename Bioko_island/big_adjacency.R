f8cddf2a2bac450d5d2012db2f016363a6bbf3be
Qian Zhang <zqstat@gmail.com>
2019-01-17 13:39:00 -0800

    Update big_adjacency.R

--- Bioko_island/big_adjacency.R
+++ Bioko_island/big_adjacency.R
@@ -1,7 +1,7 @@
 world_1k_2010_shp = rasterToPolygons(world_1k_2010)
 ad_world_1k_2010_shp = gTouches(world_1k_2010_shp, world_1k_2010_shp,  byid = TRUE)
 world_100_2010_shp = rasterToPolygons(world_100_2010)
-ad_world_100_2010_shp = gTouches(world_100_2010_shp[1:10000,], world_100_2010_shp[10001:20000,],  byid = TRUE)
+#ad_world_100_2010_shp = gTouches(world_100_2010_shp[1:10000,], world_100_2010_shp[10001:20000,],  byid = TRUE)
 
 # polyids = seq_along(world_1k_2010_shp)
 # adjMat = matrix(FALSE, ncol = length(world_1k_2010_shp), nrow = length(world_1k_2010_shp))
@@ -12,3 +12,10 @@ ad_world_100_2010_shp = gTouches(world_100_2010_shp[1:10000,], world_100_2010_sh
 #   }
 # }
 ad_world_1k_2010_shp2 = gOverlaps(world_1k_2010_shp, world_1k_2010_shp,  byid = TRUE)
+
+library(spdep)
+library(sp)
+nb <- poly2nb(world_1k_2010_shp, queen = F)
+mat <- nb2mat(nb, style="B")
+
+nb_100 <- poly2nb(world_100_2010_shp, queen = F)
