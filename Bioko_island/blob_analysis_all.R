5832dc6e20f06d02a86047875e97728148124bd8
Qian Zhang <zqstat@gmail.com>
2019-02-26 01:37:15 -0800

    Update blob_analysis_all.R

--- Bioko_island/blob_analysis_all.R
+++ Bioko_island/blob_analysis_all.R
@@ -1,16 +1,17 @@
-blob_analysis_plot(mcdi100_shp, pop_var = "PEOPLE_N", dataname = "MCDI 100m")
+par(mfrow = c(2,2))
+blob_analysis_plot(mcdi100_shp, pop_var = "PEOPLE_N", dataname = "MCDI 100m", color = 'red')
 
 mcdi1k_shp$pop_num <- as.numeric(as.character(mcdi1k_shp$pop))
-blob_analysis_plot(mcdi1k_shp, pop_var = "pop_num", dataname = "MCDI 1k")
+blob_analysis_plot(mcdi1k_shp, pop_var = "pop_num", dataname = "MCDI 1k", color = 'orange')
 
 landscan_shp <- rasterToPolygons(landscan)
 landscan_shp <- landscan_shp[landscan_shp$dblbnd>0,]
-blob_analysis_plot(landscan_shp, pop_var = "dblbnd", dataname = "Landscan 1k")
+blob_analysis_plot(landscan_shp, pop_var = "dblbnd", dataname = "Landscan 1k", color = 'dark green')
 
 world_1k_2010_shp = rasterToPolygons(world_1k_2010)
 world_1k_2010_shp <- world_1k_2010_shp[world_1k_2010_shp$AFR_PPP_2010_adj_v2>0,]
 
-blob_analysis_plot_worldpop_1k <- function(shapefile, pop_var, dataname){
+blob_analysis_plot_worldpop_1k <- function(shapefile, pop_var, dataname, color = color){
   nb <- poly2nb(shapefile, queen = F)
   mat <- nb2mat(nb, style="B", zero.policy = T)
   cell_num <- length(shapefile)
@@ -30,7 +31,7 @@ blob_analysis_plot_worldpop_1k <- function(shapefile, pop_var, dataname){
     com_cluster[i] <- length(group_information[[i]])
   }
   plot(log(com_cluster,10), log(popsum,10), main = paste(dataname, ":", clu$no, "blobs (4 directions)",sep = " "),
-       ylab = "Population in each blob", xlab = "Number of connected pixels in each blob", xaxt='n', yaxt='n', xlim = c(0, 4), ylim = c(0, 6))
+       ylab = "Population in each blob",color = color, xlab = "Number of connected pixels in each blob", xaxt='n', yaxt='n', xlim = c(0, 4), ylim = c(0, 6))
   xticks <- seq(0, 4, by=1)
   xlabels <- sapply(xticks, function(i) as.expression(bquote(10^ .(i))))
   axis(1, at=c(0, 1,2,3,4), labels=xlabels)
@@ -42,4 +43,4 @@ blob_analysis_plot_worldpop_1k <- function(shapefile, pop_var, dataname){
 
 
 
-blob_analysis_plot_worldpop_1k(world_1k_2010_shp, pop_var = "AFR_PPP_2010_adj_v2", dataname = "WorldPOP 1k")
+blob_analysis_plot_worldpop_1k(world_1k_2010_shp, pop_var = "AFR_PPP_2010_adj_v2", dataname = "WorldPOP 1k", color = 'blue')
